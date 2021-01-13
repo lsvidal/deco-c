df382b91f393e074c97a4560e8bc376ad74d68ba
Leonardo <leonardo73vidal@gmail.com>
2021-01-13 16:25:57 -0300

    Calling a handler

--- index.ts
+++ index.ts
@@ -48,14 +48,52 @@ class Greeter {
   }
 
   @Get('list')
-  list() {}
+  list() {
+    return 'list'
+  }
 
-  @Get('list/:id')
-  byId() {}
+  @Get('message')
+  getMessage(value: string) {
+    return `${this.hello} ${value}`
+  }
 }
+
+class Caller {
+  handlers = {
+    'GET': [],
+    'POST': []
+  }
+  register(c: any) {
+    if ('_d' in c) {
+      const basePath = c._d.basePath
+      c._d.routes.forEach(it => {
+        console.log(it)
+        const handler = {
+          path: `${basePath}/${it.path}`,
+          handler: c[it.name].bind(c)
+        }
+        console.log(handler)
+        this.handlers[it.method].push(handler)
+      })
+    }
+  }
+
+  call(method: string, path: string) {
+    const handler = this.handlers[method].find(it => it.path == path)
+    if (handler) {
+      const res = handler.handler()
+      console.log(res)
+    }
+  }
+}
+
 
 console.log('Instantiating class')
 const greeter = new Greeter('Hello')
 console.log(greeter)
 console.log((greeter as any)._d)
+
+const caller = new Caller()
+caller.register(greeter)
+caller.call('GET', 'poc/list')
 
